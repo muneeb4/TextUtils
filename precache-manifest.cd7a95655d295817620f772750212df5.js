b37aa2b85b4aafef7f17ae3232d58aad7a1e160c
muneeb <muneebahmed16.93@gmail.com>
2022-06-27 00:21:39 +0500

    Updates

---
+++ precache-manifest.cd7a95655d295817620f772750212df5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f72aba009016f6b9ee7f",
+    "url": "/TextUtils-React/static/css/main.b1facc92.chunk.css"
+  },
+  {
+    "revision": "f72aba009016f6b9ee7f",
+    "url": "/TextUtils-React/static/js/main.f72aba00.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/TextUtils-React/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/TextUtils-React/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "3d6cb2d2380ccec6dcdd",
+    "url": "/TextUtils-React/static/js/runtime~main.3d6cb2d2.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TextUtils-React/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f944ebd7a50577d141e11253318b7b96",
+    "url": "/TextUtils-React/index.html"
+  }
+];
